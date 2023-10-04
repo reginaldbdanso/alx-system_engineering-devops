c0bea61bb36aa28850b5f44556e3ffdffa713ac3
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-10-04 22:44:56 +0000

    Updated files 1 and 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,59 @@
-# Ensure Nginx is installed
+# Install nginx if it is not already installed
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Define the custom HTTP response header value (server's hostname)
-$server_hostname = $::hostname
-
-# Create an Nginx configuration file for the custom header
-file { '/etc/nginx/conf.d/custom_header.conf':
-  ensure  => 'file',
-  content => "server {\n  listen 80 default_server;\n  server_name _;\n  location / {\n    add_header X-Served-By \"$hostname\";\n }\n}\n",
+# allow incoming HTTP traffic on port 80
+firewall { '100 allow http access':
+  port   => 80,
+  proto  => tcp,
+  action => accept,
 }
 
-# Notify Nginx to reload its configuration when the file changes
-exec {'nginx_restart':
-  provider => shell,
-  command  => 'sudo service nginx restart'
+# write the string "Hello World!" to the file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Use sed to replace the default server block with a new server block that includes a redirect rule for /redirect_me
+file_line { 'nginx_redirect':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'location /redirect_me { return 301 https://www.youtube.com/watch?v=dQw4w9WgXcQ; }',
+  match   => 'listen \[::\]:80 default_server;',
+  ensure  => present,
+  require => Package['nginx'],
+}
+
+# Create the custom 404 error page
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page",
+}
+
+# Configure Nginx to use the custom 404 error page
+file_line { 'nginx_404':
+  path    => '/etc/nginx/sites-available/default',
+  line    => "error_page 404 /404.html;\n\tlocation = /404.html {\n\t\tinternal;\n\t}",
+  match   => 'listen \[::\]:80 default_server;',
+  ensure  => present,
+  require => Package['nginx'],
+}
+
+# Add custom HTTP response header
+file_line { 'nginx_header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $HOSTNAME;',
+  match   => 'server {',
+  ensure  => present,
+  require => Package['nginx'],
+}
+
+# Restart Nginx
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => [
+    File_line['nginx_redirect'],
+    File_line['nginx_404'],
+    File_line['nginx_header'],
+  ],
 }
